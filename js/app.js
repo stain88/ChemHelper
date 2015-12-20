0191585c6b28c1a8489d5e3c64141b7cf0e25b8f
stain88 <marcbaghdadi@gmail.com>
2015-12-20 21:42:44 +0000

    init commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+angular
+  .module('ElementsApp', ['ngResource', 'ui.bootstrap', 'ui.router', 'satellizer'])
+  .constant('API', 'https://chemistry-learner.herokuapp.com/api')
+  .config(MainRouter)
+  .config(oauthConfig);
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/home.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'views/about.html'
+    })
+    .state('learn', {
+      url: '/learn',
+      templateUrl: 'views/learn.html'
+    })
+    .state('quiz', {
+      url: '/quiz',
+      templateUrl: 'views/quiz.html'
+    });
+
+  $urlRouterProvider.otherwise('/');
+};
+
+oauthConfig.$inject = ['API', '$authProvider']
+function oauthConfig(API, $authProvider) {
+  $authProvider.facebook({
+    url: API + '/auth/facebook', 
+    clientId: '157948801229043'
+  });
+  $authProvider.github({
+    url: API + '/auth/github',
+    clientId: 'ba74c099e806a3426e66'
+  });
+}
---
+++ js/controllers/elementsController.js
@@ -0,0 +1,60 @@
+angular
+  .module('ElementsApp')
+  .controller('ElementsController', ElementsController);
+
+ElementsController.$inject = ['Element', 'Fact'];
+function ElementsController(Element, Fact) {
+  var self = this;
+  self.all = [];
+  self.selectedElement;
+  self.fact = {};
+
+  self.getElements = function() {
+    self.all = Element.query();
+  }
+
+  self.selectElement = function(element) {
+    Element.get({id: element._id}, function(ele) {
+      self.selectedElement = ele.element;
+      self.selectedElement.protonSize = Math.sqrt(ele.element.number)+20;
+      self.selectedElement.rings = rings(ele.element.electrons);
+    });
+  }
+
+  self.range = function(n) {
+    return new Array(n);
+  }
+
+  function rings(arr) {
+    var count = 0;
+    for (var i=0;i<arr.length; i++) {
+      if (arr[i]!=="") count+=1;
+    }
+    return count;
+  }
+
+  self.transform = function(n) {
+    return "rotate("+n+"deg)";
+  }
+
+  self.addFact = function() {
+    if (self.fact._id) {
+      
+      Fact.update({id: self.fact._id}, self.fact, function() {
+        self.fact = {};
+      });
+    } else {
+      self.fact.elem_id = self.selectedElement._id;
+      Fact.save(self.fact, function(fact) {
+        self.fact = {};
+      });
+    };
+  };
+
+  self.editFact = function(fact) {
+    self.fact = fact;
+  }
+
+  self.getElements();
+
+}
